30926bb1229252b007323cca65adb301403d44b7
OscarRivas99 <00005218@uca.edu.sv>
2019-11-27 17:27:32 -0600

    proof 4

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
   database: {
-    URI: 'mongodb://OscarR:root@cluster0-xxunr.mongodb.net/pictures-safe?retryWrites=true&w=majority'
-
+    URI: 'mongodb://localhost/picture-safe'
   }
 };
